bf639a2eb5ee6cbe7b65eb726a89bea0ff9e24b6
green <41323182+greeeen-dev@users.noreply.github.com>
2024-02-03 13:36:58 +0100

    changed a BUNCH of stuff

--- cogs/config.py
+++ cogs/config.py
@@ -1,28 +1,12 @@
 import discord
 from discord.ext import commands
 import ast
-#Db auto update thingy
 import json
 
-class AutoSaveDict(dict):
-    def __init__(self, file_path='data.json', *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.file_path = file_path
-        self.load_data()
+admin_ids = [356456393491873795, 549647456837828650]
 
-    def load_data(self):
-        try:
-            with open(self.file_path, 'r') as file:
-                data = json.load(file)
-            self.update(data)
-        except FileNotFoundError:
-            pass  # If the file is not found, initialize an empty dictionary
+restricted_rooms = ["test"]
 
-    def save_data(self):
-        with open(self.file_path, 'w') as file:
-            json.dump(self, file, indent=2)
-
-# Load data from db.json into an existing dictionary
 sample_db = {"rules": {
     'main': ['Be civil and follow Discord ToS and guidelines.',
               'Absolutely no NSFW in here - this is a SFW channel.',
@@ -44,13 +28,31 @@ sample_db = {"rules": {
     'test': ['test your heart out']
     }
 }
-db = AutoSaveDict(sample_db)
 
+class AutoSaveDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_path = 'data.json'
+        
+        # Ensure necessary keys exist
+        self.update({'rules':{},'rooms':{},'emojis':[],'nicknames':{},
+                     'restricted':[],'locked':[],'superlocked':[],
+                     'blocked':{},'banned':{}})
 
+        # Load data
+        self.load_data()
 
-admin_ids = [356456393491873795, 549647456837828650]
+    def load_data(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                data = json.load(file)
+            self.update(data)
+        except FileNotFoundError:
+            pass  # If the file is not found, initialize an empty dictionary
 
-restricted_rooms = ["test"]
+    def save_data(self):
+        with open(self.file_path, 'w') as file:
+            json.dump(self, file, indent=4)
 
 def is_user_admin(id):
     try:
@@ -77,11 +79,24 @@ def is_room_restricted(room):
 class Config(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+        if not hasattr(bot, 'db'):
+            self.bot.db = AutoSaveDict(sample_db)
         if not hasattr(self.bot, 'bridged_emojis'):
-            x = open('emojis.txt','r',encoding='utf-8')
-            emojis = x.read()
-            x.close()
-            self.bot.bridged_emojis = ast.literal_eval(emojis)
+            if not 'emojis' in list(self.bot.db.keys()):
+                self.bot.db.update({'emojis':[]})
+                self.bot.db.save_data()
+            self.bot.bridged_emojis = self.bot.db['emojis']
+
+    @commands.command()
+    async def make(self,ctx,*,room):
+        if not is_user_admin(ctx.author.id):
+            return await ctx.send('Only admins can create rooms!')
+        if room in list(self.bot.db['rooms'].keys()):
+            return await ctx.send('This room already exists!')
+        self.bot.db['rooms'].update({room:{}})
+        self.bot.db['rules'].update({room:[]})
+        self.bot.db.save_data()
+        await ctx.send(f'Created room `{room}`!')
     
     @commands.command(aliases=['link','connect','federate','bridge'])
     async def bind(self,ctx,*,room=''):
@@ -93,7 +108,7 @@ class Config(commands.Cog):
             room = 'main'
             await ctx.send('**No room was given, defaulting to main**')
         try:
-            data = db[f"room_{room}"]
+            data = self.bot.db['rooms'][room]
         except:
             return await ctx.send('This isn\'t a valid room. Try `main`, `pr`, `prcomments`, or `liveries` instead.')
         try:
@@ -105,12 +120,15 @@ class Config(commands.Cog):
                 return await ctx.send('Your server is already linked to this room.\n**Accidentally deleted the webhook?** `u!unlink` it then `u!link` it back.')
             index = 0
             text = ''
-            for rule in rules[room]:
-                if text=='':
-                    text = f'1. {rule}'
-                else:
-                    text = f'{text}\n{index}. {rule}'
-                index += 1
+            if len(self.bot.db['rules'][room])==0:
+                text = f'No rules exist yet for this room! For now, follow the main room\'s rules.\nYou can always view rules if any get added using `u!rule {room}`.'
+            else:
+                for rule in self.bot.db['rules'][room]:
+                    if text=='':
+                        text = f'1. {rule}'
+                    else:
+                        text = f'{text}\n{index}. {rule}'
+                    index += 1
             text = f'{text}\n\nPlease display these rules somewhere accessible.'
             embed = discord.Embed(title='Please agree to the room rules first:',description=text)
             embed.set_footer(text='Failure to follow room rules may result in user or server restrictions.')
@@ -146,11 +164,12 @@ class Config(commands.Cog):
             if resp.custom_id=='reject':
                 return
             webhook = await ctx.channel.create_webhook(name='Unifier Bridge')
-            data = db[f"room_{room}"]
+            data = self.bot.db['rooms'][room]
             guild = []
             guild.append(webhook.id)
             data.update({f'{ctx.guild.id}':guild})
-            db[f"room_{room}"] = data
+            self.bot.db['rooms'][room] = data
+            self.bot.db.save_data()
             await ctx.send('Linked channel with network!')
         except:
             await ctx.send('Something went wrong - check my permissions.')
@@ -163,7 +182,7 @@ class Config(commands.Cog):
         if not ctx.author.guild_permissions.administrator and not is_user_admin(ctx.author.id):
             return await ctx.send('You don\'t have the necessary permissions.')
         try:
-            data = db[f"room_{room}"]
+            data = self.bot.db['rooms'][room]
         except:
             return await ctx.send('This isn\'t a valid room. Try `main`, `pr`, `prcomments`, or `liveries` instead.')
         try:
@@ -177,7 +196,8 @@ class Config(commands.Cog):
                     await webhook.delete()
                     break
             data.pop(f'{ctx.guild.id}')
-            db[f"room_{room}"] = data
+            self.bot.db['rooms'][room] = data
+            self.bot.db.save_data()
             await ctx.send('Unlinked channel from network!')
         except:
             await ctx.send('Something went wrong - check my permissions.')
@@ -186,25 +206,74 @@ class Config(commands.Cog):
     @commands.command()
     async def rules(self,ctx,*,room):
         '''Displays room rules.'''
-        room = room #removed '_' + room, now works without it
         if is_room_restricted(room) and not is_user_admin(ctx.author.id):
             return await ctx.send(':eyes:')
         if room=='' or not room:
             room = 'main'
 
+        if not room in list(self.bot.db['rooms'].keys()):
+            return await ctx.send('This room doesn\'t exist! Run `u!rooms` to get a full list.')
+        
         index = 0
         text = ''
-        try:
-            for rule in rules[room]:
-                if text=='':
-                    text = f'1. {rule}'
-                else:
-                    text = f'{text}\n{index}. {rule}'
-                index += 1
-        except:
-            return await ctx.send('This isn\'t a valid room. Try `main`, `pr`, `prcomments`, or `liveries` instead.')
+        if room in list(self.bot.db['rules'].keys()):
+            rules = self.bot.db['rules'][room]
+            if len(rules)==0:
+                return await ctx.send('The room creator hasn\'t added rules yet. For now, follow `main` room rules.')
+        else:
+            return await ctx.send('The room creator hasn\'t added rules yet. For now, follow `main` room rules.')
+        for rule in rules:
+            if text=='':
+                text = f'1. {rule}'
+            else:
+                text = f'{text}\n{index}. {rule}'
+            index += 1
         embed = discord.Embed(title='Room rules',description=text)
         embed.set_footer(text='Failure to follow room rules may result in user or server restrictions.')
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def addrule(self,ctx,*,args):
+        if not is_user_admin(ctx.author.id):
+            return await ctx.send('Only admins can modify rules!')
+        try:
+            room, rule = args.split(' ',1)
+        except:
+            return await ctx.send('Rule is missing.')
+        if not room in list(self.bot.db['rules'].keys()):
+            return await ctx.send('This room does not exist!')
+        self.bot.db['rules'][room].append(rule)
+        self.bot.db.save_data()
+        await ctx.send('Added rule!')
+
+    @commands.command()
+    async def delrule(self,ctx,*,args):
+        if not is_user_admin(ctx.author.id):
+            return await ctx.send('Only admins can modify rules!')
+        try:
+            room, rule = args.split(' ',1)
+        except:
+            return await ctx.send('Rule is missing.')
+        try:
+            rule = int(rule)
+            if rule <= 0:
+                raise ValueError()
+        except:
+            return await ctx.send('Rule must be a number higher than 0.')
+        if not room in list(self.bot.db['rules'].keys()):
+            return await ctx.send('This room does not exist!')
+        self.bot.db['rules'][room].pop(rule-1)
+        self.bot.db.save_data()
+        await ctx.send('Removed rule!')
+
+    @commands.command()
+    async def rooms(self,ctx):
+        embed = discord.Embed(title=f'UniChat rooms (Total: `{len(self.bot.db["rooms"])}`)',description='Use `u!bind <room>` to bind to a room.')
+        if len(self.bot.db['rooms'])==0:
+            embed.add_field(value='No rooms here <:notlikenevira:1144718936986882088>')
+            return await ctx.send(embed=embed)
+        for room in self.bot.db['rooms']:
+            embed.add_field(name=f'`{room}` - '+str(len(self.bot.db['rooms'][room]))+' servers',value='Descriptions coming soon!',inline=False)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -217,9 +286,8 @@ class Config(commands.Cog):
         else:
             self.bot.bridged_emojis.append(ctx.guild.id)
             await ctx.send('All members can now use your emojis!')
-        x = open('emojis.txt','w+',encoding='utf-8')
-        x.write(f'{self.bot.bridged_emojis}')
-        x.close()
+        self.bot.db['emojis'] = self.bot.bridged_emojis
+        self.bot.db.save_data()
     
 def setup(bot):
     bot.add_cog(Config(bot))
