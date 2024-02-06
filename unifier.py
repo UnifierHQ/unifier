d162f6e9ce00a9eb38aebd435809ba19a09ce774
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-06 20:26:49 +0100

    GNU license + music gone

--- unifier.py
+++ unifier.py
@@ -1,3 +1,21 @@
+"""
+Unifier - A "simple" bot to unite Discord servers with webhooks
+Copyright (C) 2024  Green and ItsAsheer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import discord
 from discord.ext import commands
 import aiohttp
