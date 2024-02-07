212d107705d2614327998fe26d90f42e71e05d10
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-07 15:49:39 +0100

    about update

--- cogs/config.py
+++ cogs/config.py
@@ -435,9 +435,9 @@ class Config(commands.Cog):
     @commands.command()
     async def about(self,ctx):
         embed = discord.Embed(title="Unifier and Unified Chat",description="Unify servers, make worthwhile conversations.",color=0xed4545)
-        embed.add_field(name="Developers",value="@green.\n@itsasheer")
-        embed.add_field(name="PFP made by",value="@green.\n@thegodlypenguin")
-        embed.set_footer(text="Version v0.3.0 (Beta)")
+        embed.add_field(name="Developers",value="@green.\n@itsasheer",inline=False)
+        embed.add_field(name="PFP made by",value="@green.\n@thegodlypenguin",inline=False)
+        embed.set_footer(text="Version v0.3.5 (Beta)")
         await ctx.send(embed=embed)
 
     @commands.command()
