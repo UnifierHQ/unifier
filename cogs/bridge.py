f4b4becfb7d46b023f31490a382f3d9177dd1f46
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-19 11:16:40 +0100

    fix

--- cogs/bridge.py
+++ cogs/bridge.py
@@ -555,7 +555,7 @@ class Bridge(commands.Cog):
             except:
                 return
 
-        await interaction.response.edit_message('Generating...', components=None)
+        await interaction.response.edit_message(content='Generating...', components=None)
         msgid = msg.id
         try:
             if interaction.custom_id=='think':
