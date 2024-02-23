1c4e976e87b05ea8e39a03fd96243f721c326ccf
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-23 21:30:19 +0100

    fixes

--- cogs/bridge_revolt.py
+++ cogs/bridge_revolt.py
@@ -8,6 +8,8 @@ import json
 with open('config.json', 'r') as file:
     data = json.load(file)
 
+owner = data['owner']
+
 class Revolt(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -23,11 +25,12 @@ class Revolt(commands.Cog):
         if self.bot.revolt_client is None:
             async with aiohttp.ClientSession() as session:
                 self.bot.revolt_client = self.Client(session, data['revolt_token'])
+                print('booting revolt client')
                 await self.bot.revolt_client.start()
 
     @commands.command(hidden=True)
     async def send_to_revolt(self,ctx,*,message):
-        if not ctx.author.id==self.bot.owner:
+        if not ctx.author.id==owner:
             return
         server = self.bot.revolt_client.get_server('01HDS71G78AT18B9DEW3K6KXST')
         channel = server.get_channel('01HDS71G78TTV3J3HMX3FB180Q')
