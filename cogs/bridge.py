1dcc80bad994a7b780fb8f6cc3e276da53236e58
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-24 17:27:40 +0100

    debug

--- cogs/bridge.py
+++ cogs/bridge.py
@@ -2077,6 +2077,8 @@ class Bridge(commands.Cog, name=':link: Bridge'):
                                 if f'{message.reference.message_id}' in f'{self.bot.bridged[key]}':
                                     msg_data = self.bot.bridged_external[f'{key}']['revolt']
                                     break
+                            if not msg_data:
+                                raise ValueError()
                     except:
                         raise
                         for key in self.bot.bridged_external:
