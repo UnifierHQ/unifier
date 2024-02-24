17df5826b852498fded440043a6f4d4a2d183a05
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-24 12:37:01 +0100

    reply fix

--- cogs/bridge.py
+++ cogs/bridge.py
@@ -2074,10 +2074,10 @@ class Bridge(commands.Cog, name=':link: Bridge'):
                 msg_data = None
                 if not message.reference is None:
                     try:
-                        msg_data = self.bot.bridged_external[f'{message.id}']
+                        msg_data = self.bot.bridged_external[f'{message.reference.message_id}']
                     except:
                         for key in self.bot.bridged_external:
-                            if f'{message.id}' in str(self.bot.bridged_external[key]['revolt']):
+                            if f'{message.reference.message_id}' in str(self.bot.bridged_external[key]['revolt']):
                                 msg_data = self.bot.bridged_external[f'{key}']['revolt']
                                 break
                 if not msg_data:
