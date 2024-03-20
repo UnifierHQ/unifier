9fd801a98c83689ba521d1fe3692feb4c7675864
green. <41323182+gweeeen@users.noreply.github.com>
2024-03-20 13:57:00 +0100

    fix

--- cogs/bridge.py
+++ cogs/bridge.py
@@ -1835,10 +1835,6 @@ class Bridge(commands.Cog, name=':link: Bridge'):
         except:
             if not msgid:
                 return await ctx.send('No message detected')
-            try:
-                msgid = int(msgid)
-            except:
-                return await ctx.send('No message detected')
         msg: UnifierMessage = await self.bot.bridge.fetch_message(msgid)
         text = f'Author: {msg.author_id}\nGuild: {msg.guild_id}\nSource: {msg.source}\nParent is webhook: {msg.webhook}\n\nCopies (samesource):'
         for key in msg.copies:
