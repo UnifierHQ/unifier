a978a6a6b39a691e59e56ce09bb5ad7dd60fd7fb
green <41323182+greeeen-dev@users.noreply.github.com>
2024-05-01 21:19:03 +0200

    spypet notification (still not done)

--- cogs/spypetguard.py
+++ cogs/spypetguard.py
@@ -5,7 +5,8 @@ import aiohttp
 import json
 import requests
 
-class SpySlayer: # NOT THE COG ITSELF, JUST THE SCANNER AND ID CACHER.
+class SpySlayer:
+    """A spy.pet scraper bot detector."""
     def __init__(self,bot,logger):
         self.bot = bot
         self.ids = {}
@@ -51,15 +52,6 @@ class SpySlayer: # NOT THE COG ITSELF, JUST THE SCANNER AND ID CACHER.
         self.bot.db['spybot'].append(guild.id)
         return True
 
-
-"""
-if the ban is permanent (expire time = 0), don't do anything
-
-if the ban is temporary (expire time > 0), warn the servers but don't change the ban, but add the server id to self.bot.db['spybot']
-
-if there are no bans, warn the servers and add a permanent ban and the server id to self.bot.db['spybot']
-"""
-
 class SpyPetGuard(commands.Cog, name="SpyPet Guard"):
     """Cog to check for potential web scraper users on member join."""
 
@@ -96,31 +88,82 @@ class SpyPetGuard(commands.Cog, name="SpyPet Guard"):
                 room = self.bot.config['main_room']
                 if room in list(self.bot.db['rooms'].keys()):
                     for guild_id in list(room.keys()):
-                        guild = self.bot.get_guild(int(guild_id))
-                        webhook: discord.Webhook = await guild.fetch_webhook(room[f'{member.guild.id}'][0])
-                        if guild.id==member.guild.id:
+                        try:
+                            webhook: discord.Webhook = await self.bot.fetch_webhook(room[f'{guild_id}'][0])
+                        except:
+                            continue
+                        if int(guild_id)==member.guild.id:
                             embed = discord.Embed(
                                 color=0xff0000,
                                 title="WARNING: A scraper bot was detected!",
                                 description="A scraper bot was detected in your server. Unifier has automatically "+
-                                            "disconnected your server for users' safety.\n\n"+
+                                            "global banned your server for users' safety.\n\n"+
                                             "Bots detected:\n"+
                                             f"- <@{member.id}>\n\n"+
                                             "Source: [kickthespy.pet ID list](https://kickthespy.pet/ids)"
-
-
-
                             )
-                            await webhook.send(
-                                avatar_url=self.bot.user.avatar.url if self.bot.user.avatar else None,
-                                username=self.bot.user.name + ' (system)',
-                                embed=embed
+                            # note to devs: convert each step to individual fields if things get complicated
+                            embed.add_field(
+                                name='What now?',
+                                value='1. Ban all users shown above, as they are scraper bots.\n'+
+                                      '2. Run `u!spyscan`.\n'+
+                                      '3. If the bots are no longer detected, the ban will be lifted, unless '+
+                                      'the server has another ongoing ban.',
+                                inline=False
                             )
+                            await webhook.channel.send(embed=embed)
                         else:
                             embed = discord.Embed(
                                 color=0xff0000,
-                                description="A scraper bot was found in a server!"
+                                title='Server removed',
+                                description="A scraper bot was found in a server! The server was removed from the "+
+                                            f"{self.bot.user.global_name} network and they have been informed."
                             )
+                            await webhook.send(
+                                avatar_url=self.bot.user.avatar.url if self.bot.user.avatar else None,
+                                username=self.bot.user.global_name + ' (system)',
+                                embed=embed
+                            )
+
+    @commands.command()
+    async def spyscan(self,ctx):
+        results = await self.bot.spyslayer.scan(ctx.guild)
+        if len(results[f'{ctx.guild.id}'])==0:
+            # No bots, ban can be lifted. Check for an existing ban first.
+            if ctx.guild.id in self.bot.db['spybot']:
+                self.bot.db['spybot'].remove(ctx.guild.id)
+                embed = discord.Embed(title="Scraper scan results",
+                                      description="**No bots detected**\nNo scraper bots were detected. " +
+                                                  f"Your access to {self.bot.user.global_name} has been " +
+                                                  "restored successfully!",
+                                      color=discord.Color.greyple()
+                                      )
+                if str(ctx.guild.id) in self.bot.db['banned']:
+                    if self.bot.db['banned'][f'{ctx.guild.id}'] > 0:
+                        # Guild is temporarily banned, do not lift ban
+                        embed = discord.Embed(title="Scraper scan results",
+                                              description="**No bots detected**\nNo scraper bots were detected, "+
+                                                          "however your server still has an ongoing ban. Your access "+
+                                                          f"to {self.bot.user.global_name} will be restored once the "+
+                                                          "ban is lifted or expires.",
+                                              color=0xffcc00
+                        )
+                    else:
+                        self.bot.db['banned'].pop(f'{ctx.guild.id}')
+            else:
+                embed = discord.Embed(title="Scraper scan results",
+                                      description="**No bots detected**\nNo scraper bots were detected. " +
+                                                  "All is well!",
+                                      color=discord.Color.greyple()
+                                      )
+        else:
+            embed = discord.Embed(title="Scraper scan results",
+                                  description="**Bots detected**\nOne or more scrapers were detected. Server will " +
+                                              "be global banned for safety.",
+                                  color=0xff0000
+                                  )
+            await self.bot.spyslayer.disconnect(ctx.guild)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(SpyPetGuard(bot))
