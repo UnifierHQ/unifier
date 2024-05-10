490cb56b3b25e346b30ac8eb2bfd1f13dc21561e
Altexx87 <140397732+NullyIsHere@users.noreply.github.com>
2024-05-10 20:40:19 +0200

    Code With Me.
    FT green. & ItsAsheer

--- cogs/badge.py
+++ cogs/badge.py
@@ -1,113 +1,86 @@
-"""
-Unifier - A sophisticated Discord bot uniting servers and platforms
-Copyright (C) 2024  Green, ItsAsheer
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 import discord
 from discord.ext import commands
 from utils import log
+from enum import Enum
+
+
+class UserRole(Enum):
+    OWNER = "owner"
+    ADMIN = "admin"
+    MODERATOR = "moderator"
+    TRUSTED = "trusted"
+    BANNED = "banned"
+    USER = "user"
 
 class Badge(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-        self.logger = log.buildlogger(self.bot.package,'badge',self.bot.loglevel)
+        self.logger = log.buildlogger(self.bot.package, 'badge', self.bot.loglevel)
+        self.embed_colors = {
+            UserRole.OWNER: (
+                self.bot.colors.greens_hair if self.bot.user.id==1187093090415149056 else self.bot.colors.unifier
+            ),
+            UserRole.ADMIN: discord.Color.green(),
+            UserRole.MODERATOR: discord.Color.purple(),
+            UserRole.TRUSTED: self.bot.colors.gold,
+            UserRole.BANNED: discord.Color.red(),
+            UserRole.USER: discord.Color.blurple()
+        }
 
     @commands.command()
     async def badge(self, ctx):
-        user = "user"
-        if ctx.author.id in self.bot.db['banned']:
-            user = "banned"
-        if ctx.author.id in self.bot.trusted_group:
-            user = "trusted"
-        if ctx.author.id in self.bot.moderators:
-            user = "moderator"
-        if ctx.author.id in self.bot.admins:
-            user = "admin"
-        if ctx.author.id == self.bot.config['owner']:
-            user = "owner"
-        if user == "owner":
-            embed = discord.Embed(
-                title="Unifier",
-                description=f"<@{ctx.author.id}> is a Unifier **owner**.",
-                color=self.bot.colors.greens_hair  # You can change the color to your preference
-            )
-        elif user == "admin":
-            embed = discord.Embed(
-                title="Unifier",
-                description=f"<@{ctx.author.id}> is a Unifier **admin**.",
-                color=self.bot.colors.green  # You can change the color to your preference
-            )
-        elif user == "moderator":
-            embed = discord.Embed(
-                title="Unifier",
-                description=f"<@{ctx.author.id}> is a Unifier **moderator**.",
-                color=self.bot.colors.purple  # You can change the color to your preference
-            )
-        elif user == "trusted":
-            embed = discord.Embed(
-                title="Unifier",
-                description=f"<@{ctx.author.id}> is a Unifier **trusted user**.",
-                color=self.bot.colors.gold  # You can change the color to your preference
-            )
-        elif user == "banned":
-            embed = discord.Embed(
-                title="Unifier",
-                description=f"<@{ctx.author.id}> is a Unifier **banned user**.",
-                color=self.bot.colors.red  # You can change the color to your preference
-            )
-            embed.set_footer(text="L bozo") # :nevheh:
-        else:
-            embed = discord.Embed(
-                title="Unifier",
-                description=f"<@{ctx.author.id}> is a Unifier **user**.",
-                color=self.bot.colors.blurple  # You can change the color to your preference
-            )
+        user_role = self.get_user_role(ctx.author.id)
+        embed = discord.Embed(
+            title="Unifier",
+            description=f"<@{ctx.author.id}> is a Unifier **{user_role.value}**.",
+            color=self.embed_colors[user_role]
+        )
+        # Best easter egg in the world
+        #TODO ADD EASTER EGG L BOZO
+
         await ctx.send(embed=embed)
 
     @commands.command(hidden=True)
-    async def trust(self, ctx, action, user):
-        if not ctx.user.id in self.bot.admins:
-            return ctx.send("Nah, you can't access Unifier Trust n' safety zone! L")
-        
-        userid = int(userid.replace('<@','',1).replace('!','',1).replace('>','',1))
-        
-        if action == "add":
-            self.bot.trusted_group.append(userid)
-            embed = discord.Embed(
-                title="Unifier"
-                description=f"Added user <@{userid}> ({userid}) to the trust group!"
-                self.bot.colors.green
-            )
-        if action == "remove":
-            self.bot.trusted_group.remove(userid)
-            embed = discord.Embed(
-                title="Unifier"
-                description=f"Removed user <@{userid}> ({userid}) to the trust group!"
-                self.bot.colors.red
-            )
-        elif:
-            embed = discord.Embed(
-                title="Unifier"
-                description=f"Action not found!"
-                self.bot.colors.purple
-            )
-            
-        
+    async def trust(self, ctx, action, user: discord.User):
+        if ctx.author.id not in self.bot.admins:
+            return await ctx.send("You don't have permission to use this command.")
+
+        action = action.lower()
+        if action not in ['add', 'remove']:
+            return await ctx.send("Invalid action. Please use 'add' or 'remove'.")
+
+        trusted_group = self.bot.trusted_group
+        if action == 'add':
+            if user.id not in trusted_group:
+                trusted_group.append(user.id)
+        elif action == 'remove':
+            if user.id in trusted_group:
+                trusted_group.remove(user.id)
+
+        self.bot.trusted_group = trusted_group
+        self.bot.save_trusted_group()
+
+        user_role = UserRole.TRUSTED if action == 'add' else UserRole.USER
+        embed = discord.Embed(
+            title="Unifier",
+            description=f"{'Added' if action == 'add' else 'Removed'} user {user.mention} from the trust group.",
+            color=self.embed_colors[user_role],
+        )
         await ctx.send(embed=embed)
+
+    def get_user_role(self, user_id):
+        if user_id in self.bot.config['owner']:
+            return UserRole.OWNER
+        elif user_id in self.bot.admins:
+            return UserRole.ADMIN
+        elif user_id in self.bot.moderators:
+            return UserRole.MODERATOR
+        elif user_id in self.bot.trusted_group:
+            return UserRole.TRUSTED
+        elif user_id in self.bot.db['banned']:
+            return UserRole.BANNED
+        else:
+            return UserRole.USER
 
 def setup(bot):
     bot.add_cog(Badge(bot))
--- cogs/sysmgr.py
+++ cogs/sysmgr.py
@@ -15,7 +15,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import re
 
 import discord
 from discord.ext import commands
@@ -29,16 +28,20 @@ import os
 import sys
 import traceback
 import io
+import base64
+import re
+import ast
 import importlib
 
-class Colors:
+class Colors: # format: 0xHEXCODE
     greens_hair = 0xa19e78
     unifier = 0xed4545
     green = 0x2ecc71
     dark_green = 0x1f8b4c
     purple = 0x9b59b6
     red = 0xe74c3c
-    blurple = 0x7289da
+    blurple = 0x7289da # TODO add gold color!
+    gold = 0xd4a62a
 
 def cleanup_code(content):
     if content.startswith('```') and content.endswith('```'):
@@ -74,6 +77,7 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
 
         if not hasattr(self.bot, 'colors'):
             self.bot.colors = Colors
+            self.bot.colors.unifier = ast.literal_eval(f"0x{self.bot.config['main_color']}")
         if not hasattr(self.bot, 'pid'):
             self.bot.pid = None
         if not hasattr(self.bot, 'trusted_group'):
@@ -82,7 +86,7 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
                 tgroup.append(i)
             for i in self.bot.moderators:
                 tgroup.append(i)
-            tgroup = list(set(test_list)) # Remove duplicates, as green would be 3 times and ItsAsheer 2.
+            tgroup = list(set(tgroup)) # Remove duplicates, as green would be 3 times and ItsAsheer 2.
             self.bot.trusted_group = tgroup
         if not hasattr(self.bot, 'loglevel'):
             self.bot.loglevel = logging.DEBUG if self.bot.config['debug'] else logging.INFO
@@ -146,7 +150,7 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
                     break
         if not plugin_name:
             return
-        if plugin_name=='system':
+        if plugin_name == 'system':
             return
         if not info:
             raise ValueError('Invalid plugin')
@@ -190,6 +194,7 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
                 await ctx.author.send(
                     f'```py\n{e.__class__.__name__}: {e}\n```\nIf this is a KeyError, it is most likely a SyntaxError.')
                 return
+            token_start = base64.b64encode(self.bot.user.id)
             try:
                 with redirect_stdout(stdout):
                     # ret = await func() to return output
@@ -197,12 +202,17 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
             except:
                 value = await self.bot.loop.run_in_executor(None, lambda: stdout.getvalue())
                 await ctx.send('An error occurred while executing the code.', reference=ctx.message)
+                if token_start in value:
+                    return await ctx.author.send('The error contained your bot\'s token, so it was not sent.')
                 await ctx.author.send(f'```py\n{value}{traceback.format_exc()}\n```')
             else:
                 value = await self.bot.loop.run_in_executor(None, lambda: stdout.getvalue())
+                if token_start in value:
+                    return await ctx.send('The output contained your bot\'s token, so it was not sent.')
                 if value == '':
                     pass
                 else:
+                    #  here, cause is if haves value
                     await ctx.send('```%s```' % value)
         else:
             await ctx.send('Only the owner can execute code.')
@@ -255,7 +265,7 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
         pluglist = [plugin for plugin in os.listdir('plugins') if plugin.endswith('.json')]
         if not plugin:
             offset = page * 20
-            embed = discord.Embed(title='Unifier Plugins', color=0xed4545)
+            embed = discord.Embed(title='Unifier Plugins', color=self.bot.colors.unifier)
             text = ''
             if offset > len(pluglist):
                 page = len(pluglist) // 20 - 1
@@ -284,7 +294,12 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
                 pluginfo = json.load(file)
         else:
             return await ctx.send('Could not find extension!')
-        embed = discord.Embed(title=pluginfo["name"], description="Version " + pluginfo['version'] + ' (`' + str(pluginfo['release']) + '`)\n\n' + pluginfo["description"], color=0xed4545)
+        embed = discord.Embed(
+            title=pluginfo["name"],
+            description=("Version " + pluginfo['version'] + ' (`' + str(pluginfo['release']) + '`)\n\n' +
+                         pluginfo["description"]),
+            color=self.bot.colors.unifier
+        )
         if plugin == 'system':
             embed.description = embed.description + '\n# SYSTEM PLUGIN\nThis plugin cannot be uninstalled.'
         try:
@@ -321,7 +336,7 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
             pass
         if not extension:
             offset = page * 20
-            embed = discord.Embed(title='Unifier Extensions', color=0xed4545)
+            embed = discord.Embed(title='Unifier Extensions', color=self.bot.colors.unifier)
             text = ''
             extlist = list(self.bot.extensions)
             if offset > len(extlist):
@@ -350,7 +365,11 @@ class SysManager(commands.Cog, name=':wrench: System Manager'):
             ext_info = self.bot.cogs[list(self.bot.cogs)[index]]
         else:
             return await ctx.send('Could not find extension!')
-        embed = discord.Embed(title=ext_info.qualified_name, description=ext_info.description, color=0xed4545)
+        embed = discord.Embed(
+            title=ext_info.qualified_name,
+            description=ext_info.description,
+            color=self.bot.colors.unifier
+        )
         if (extension == 'cogs.sysmgr' or extension == 'cogs.lockdown' or
                 extension == 'sysmgr' or extension == 'lockdown'):
             embed.description = embed.description + '\n# SYSTEM MODULE\nThis module cannot be unloaded.'
