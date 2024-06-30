39523fdea914ca65b47c7f17514a04b6167c3385
green <41323182+greeeen-dev@users.noreply.github.com>
2024-06-30 17:10:16 +0200

    Update config.py

--- cogs/config.py
+++ cogs/config.py
@@ -556,7 +556,7 @@ class Config(commands.Cog, name=':construction_worker: Config'):
 
             if interaction.data['custom_id']=='bind':
                 await msg.edit(embed=embed, view=ui.MessageComponents())
-                await resp.response.defer(with_message=True)
+                await interaction.response.defer(with_message=True)
                 break
             if interaction.data['custom_id']=='selection':
                 channels_enabled = []
@@ -576,7 +576,7 @@ class Config(commands.Cog, name=':construction_worker: Config'):
             self.bot.db['rooms'][roomname].update({f'{ctx.guild.id}': guild})
 
         await self.bot.loop.run_in_executor(None, lambda: self.bot.db.save_data())
-        await resp.edit_original_message(
+        await interaction.edit_original_message(
             content=f'# {self.bot.ui_emojis.success} Linked channels to Unifier network!\nYou can now send messages to the Unifier network through the channels. Say hi!')
 
     @commands.command(description='Displays room rules for the specified room.')
