b9b6c3bb9d87c6ca17dd132d1f17212369e1ce3c
green. <41323182+greeeen-dev@users.noreply.github.com>
2024-07-05 15:01:37 +0200

    useremoji support

--- cogs/bridge.py
+++ cogs/bridge.py
@@ -1629,7 +1629,8 @@ class UnifierBridge:
                         'bridge': {
                             'name': msg_author,
                             'avatar': url,
-                            'color': color
+                            'color': color,
+                            'emoji': useremoji
                         },
                         'files': files,
                         'embeds': (
