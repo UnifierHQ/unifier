7157ce07dd19db95408ad2cd6f20325ea33f25bf
green <41323182+greeeen-dev@users.noreply.github.com>
2024-09-07 12:45:56 +0200

    run.bat fix

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
-py -3 -V >nul 2>&1 && (
-    py -3 ./boot/bootloader.py
-) || (
-    echo Could not find a Python 3 installation.
+@echo off
+py -3 -V >nul 2>&1
+
+if NOT ERRORLEVEL 0 (
+  echo on
+  echo Could not find a Python 3 installation.
+) else (
+  py -3 boot/bootloader.py
 )
