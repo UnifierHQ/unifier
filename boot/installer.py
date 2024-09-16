18fe1fad8a21d8955747b535674a87cf4978acb8
green <41323182+greeeen-dev@users.noreply.github.com>
2024-09-17 00:12:40 +0200

    fix

--- boot/installer.py
+++ boot/installer.py
@@ -150,6 +150,7 @@ with open('config.toml', 'rb') as file:
     config = tomli.load(file)
 
 config['roles']['owner'] = user_id
+config['moderation']['home_guild'] = server_id
 
 with open('config.toml', 'wb') as file:
     tomli_w.dump(config, file)
