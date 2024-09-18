10cd57e0ca5fdb1abade10f747786bc73a81b42a
green <41323182+greeeen-dev@users.noreply.github.com>
2024-09-18 17:55:58 +0200

    do not prompt setup again

--- cogs/setup.py
+++ cogs/setup.py
@@ -474,6 +474,15 @@ class Setup(commands.Cog):
 
             if skip:
                 ignore_error = True
+
+                with open('.install.json') as file:
+                    install_data = json.load(file)
+
+                install_data['setup'] = True
+
+                with open('.install.json', 'w') as file:
+                    json.dump(install_data, file)
+
                 return await setup_dialog.finish(skipped=True)
 
             if not self.bot.setup_lock:
