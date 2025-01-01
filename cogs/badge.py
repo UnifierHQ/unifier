c10aefd2a9064a1243691aa2541d4c093350f512
Sam <sam065819@outlook.com>
2025-01-01 02:38:41 +0100

    -
    -

--- cogs/badge.py
+++ cogs/badge.py
@@ -140,7 +140,6 @@ class Badge(commands.Cog, name=':medal: Badge'):
 #
 # (jokes aside happy new year)
 
-# ItsAsheer -> green. : Its bc new update to badge.py soon :eyes:, and bc its not a critical feature (imagine i broke bridge.py. We could create an easter egg file where we leave comments :)
-
+# ItsAsheer --> green. : Its bc new update to badge.py soon :eyes:, and bc its not a critical feature (imagine i broke bridge.py. We could create an easter egg file where we leave comments :)
 def setup(bot):
     bot.add_cog(Badge(bot))
